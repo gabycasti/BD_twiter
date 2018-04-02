7fab8864600377dba2f07ea73b4821e8a3681c24
gabycasti <castillg@gmail.com>
2018-04-02 17:35:14 -0300

    validacion formulario registro de usuario

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,9 +5,10 @@
 
 
 $(document).ready(function(){
+$("#correo").val("");
+$("#clave").val("");
 
 $("#btn-login").click(function(){
-        
         //Función ingresar usuario y contraseña
         var usuarios = db.ref('usuarios');
         usuarios.on('value',function(ss){
@@ -16,7 +17,7 @@ $("#btn-login").click(function(){
             var logeado = 0;
             usr = Object.keys(usuario); //ASOCIAR LOS DATOS DEL OBJETO A UNA VARIABLE
             for(i=0; i<usr.length; i++){
-                if(usuario[usr[i]].correo == $("#correo").val() && usuario[usr[i]].clave == $("#clave").val())
+                if(usuario[usr[i]].correo == $("#correo").val() && usuario[usr[i]].clave == $("#clave").val() )
                 {
                   //VARIABLE DE SESION PARA GUADRA EL NICK
                   sessionStorage['usuarioLogueado'] = usr[i];
@@ -27,14 +28,33 @@ $("#btn-login").click(function(){
               document.location.replace('twits.html')
             }else 
               {
-                alert("Ese usuario no existe o la contraseña esta incorrecta")
+                $("#datos_error").html("Ese usuario no existe o la contraseña esta incorrecta");
+                $("#datos_error").show();
+
             }
         })
     })
 
+$("#clave").keypress(function(){
+  $("#datos_error").hide();
+  var usuario = $("#correo").val();
+  if(usuario == ""){
+    $("#usuario_incorrecto").html("Introduzca su email");
+  }
+});
+
+$("#correo").keypress(function(){
+  $("#datos_error").hide();
+  $("#usuario_incorrecto").hide();
+})
+
+
 $("#btn-registro").click(function(){
    document.location.replace('registro.html')
+  });
 
+$("#portafolio").click(function(){
+  location.replace("https://gabycasti.github.io/portafolio/");
 })
 
 });
--- assets/js/register.js
+++ assets/js/register.js
@@ -1,3 +1,7 @@
+var foto= '';
+var nombre = '';
+var nick = '';
+
 $(document).ready(function(){
   // Habilitarlo a través de la página de firebase en el link autenticación
   // Habilitar al final donde dice anónimo
@@ -23,14 +27,15 @@ $(document).ready(function(){
 
   // Rescatar información del formulario crear perfil
   $('#btn-registro').click(function(){
+var expresion= /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
 // haciendo referencia a el campo usuarios de la base de datos
     var usuarios = db.ref('usuarios');
       var nombre = $('#nombre').val();
       var nick = $('#nick').val();
       var correo = $('#correo').val();
       var clave = $('#clave').val();
-  
-// Creo un objeto para almacenar los datos de un usuario
+
+   // Creo un objeto para almacenar los datos de un usuario
 
   //var password2 = $('#password2').val();
   // Creo un objeto para almacenar los datos de un usuario
@@ -39,7 +44,43 @@ $(document).ready(function(){
       usuario.nombre=nombre;
       usuario.correo = correo;
       usuario.clave= clave;
-      usuario.foto=rutaFoto;
+      usuario.foto=foto;
+
+//Validando los campos de registro
+/* El metodo .test nos valida si se cumple o no la expresión regular
+    * En este caso lo esta negando es decir si no se cumple
+    * Me retorna un valor booleano verdadero o false      
+    */
+        if(nombre == ''){
+           $('#mensaje_nombre').fadeIn();
+           return false;
+        }else{
+            $('#mensaje_nombre').fadeOut();
+            if(nick == ''){
+                $("#mensaje_nick").fadeIn();
+                 return false;
+        }else{
+            $('#mensaje_nick').fadeOut();
+            if(correo == '' || !expresion.test(correo)){
+              $('#mensaje_correo' ).fadeIn();
+               return false;
+        }else{
+            $('#mensaje_correo').fadeOut();
+            if(clave == ''){
+                $("#mensaje_clave").fadeIn();
+                 return false;
+        }else{
+           $('#mensaje_clave').fadeOut();
+          if(foto == ""){
+            $("#mensaje_foto").fadeIn();
+                 return false;
+          }
+        }
+       }
+      }
+    }
+
+  
 //Llamo al campo referencia de usuarios de la base de datos que es nick 
 // Guardo con set el objeto usuario con todos los datos de los usuarios
 //llamo al campo referencia de usuarios de la base de datos que es nick 
@@ -53,6 +94,29 @@ $(document).ready(function(){
       document.location.replace('twits.html')
       //cargaDatos();
     })
+
+
+$("#nombre").keypress(function(){
+     $('#mensaje_nombre').hide();
+  });
+  
+
+
+$("#nick").keypress(function(){
+     $('#mensaje_nick').hide();
+  });
+
+$("#correo").keypress(function(){
+     $('#mensaje_correo').hide();
+  });
+
+$("#clave").keypress(function(){
+     $('#mensaje_clave').hide();
+  });
+
+$("#foto").click(function(){
+     $('#mensaje_foto').hide();
+  });
   
 });
 
@@ -71,8 +135,8 @@ $(document).ready(function(){
           //mensajeFinalizado(uploadTask.snapshot.downloadURL, uploadTask.snapshot.totalBytes);
           //Guardo la ruta donde quedo registrada la foto en el servidor
           //snapshot.downloadURL es el equivalente a ss recasta la foto
-          rutaFoto = uploadTask.snapshot.downloadURL
-          console.log('Subida completada: '+rutaFoto);
+          foto = uploadTask.snapshot.downloadURL
+          console.log('Subida completada: '+foto);
           
         }
       );
